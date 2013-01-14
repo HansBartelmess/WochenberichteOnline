be6fc2764e5907bb9f4cc10cf1324c2e7ec0fc47
HansBartelmess <h.bartelmess@googlemail.com>
2013-01-14 07:42:59 +0100

    show seite angefangen

--- showReport.php
+++ showReport.php
@@ -1,1 +1,16 @@
+<?php // Einstiegsseite
+require_once('include.php');
+EnsureLogin();
+
+$smarty = new Smarty;
+
+CreateMenu($smarty);
+
+   R::load($reports);
+
+
+$smarty->display('templates/showReport.tpl');
+R::close();
+
+?>
 
