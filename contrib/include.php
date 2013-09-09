418425f5547c5b1b2fac1c1fd3c560ed9da32a9d
simonschubert0 <root@blackbox.sowi.uni-mannheim.de>
2013-09-09 15:26:14 +0200

    test2

--- contrib/include.php
+++ contrib/include.php
@@ -8,3 +8,6 @@ require_once('ProcessHelper/folder.class.php');
 require_once('ProcessHelper/process.class.php');
 
 ?>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
