435f5c60c6a4f3629575e0a4744e85dbb135bc14
HansBartelmess <h.bartelmess@googlemail.com>
2013-09-11 16:23:45 +0200

    comments

--- createReport.php
+++ createReport.php
@@ -7,6 +7,9 @@ $smarty = new Smarty;
 CreateMenu($smarty);
 
 if(isset($_POST["submit"]) && isset($_POST['reportNumber']) && isset($_POST['division']) && isset($_POST['startDate']) && (isset($_POST['company']) || isset($_POST['training']) || isset($_POST['school'])))  {
+
+
+   //Finde den aktiven user
    $user = R::findOne('user', 'username = ?', array($_SESSION['username']));
 
    $reports = R::dispense('reports');
@@ -16,7 +19,10 @@ if(isset($_POST["submit"]) && isset($_POST['reportNumber']) && isset($_POST['div
    $reports->company = $_POST['company'];
    $reports->training = $_POST['training'];
    $reports->school = $_POST['school'];
+
+   //weise dem Bericht einem benutzer zu
    $user->ownReport[] = $reports;
+   //speichere den Bericht mit dem Benutzer
    R::store($user);
 
    $smarty->assign('saved', true);
