1d5233b4d580a7e9a12189554ad9dc818435f1b6
HansBartelmess <h.bartelmess@googlemail.com>
2013-09-20 13:49:28 +0200

    leider auch net

--- getReports2.php
+++ getReports2.php
@@ -4,8 +4,7 @@ require_once('functions.php');
 
 
 
-$sql = 'select * from reports reports,user user WHERE user.username = '. $_POST['username'] .' && user.id = reports.user_id && user.jobid = '. $_POST['sessjobid'].' && reports.division = '. $_POST['sessdept'] .' ';
-
+$sql = "select * from reports reports,user user WHERE user.username = '".$_POST['username']."' && user.id = reports.user_id && user.jobid = '".$_POST['sessjobid']."' && reports.division = '".$_POST['sessdept']."'";
 $ergebnis = mysql_query($sql); 
 while($row = mysql_fetch_object($ergebnis)) { 
    echo "$row->reportNumber"; 
