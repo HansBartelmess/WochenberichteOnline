aeaabede944f14499290f0d29462f0ddffa5a4a3
HansBartelmess <h.bartelmess@googlemail.com>
2014-01-30 14:11:11 +0100

    session hinzugefügt und id für abfrage

--- getReports_bio.php
+++ getReports_bio.php
@@ -1,15 +1,15 @@
 <?php
-
+   session_start();
 	include('functions.php');
 	
 	header('Content-type: text/json');
 	header('Content-type: application/json');
 	
-	$sql = "SELECT * FROM bio_report WHERE nachweis='".$_GET['nachweis']."' ";
+	$sql = "SELECT * FROM bio_report WHERE nachweis='".$_GET['nachweis']."' && uid='".$_SESSION['id']."' ";
 	$res = sql($sql);
 	
 	while($rw=mysql_fetch_array($res)){
-		echo $rw['data'];
+      echo $rw['data'];
 	}
 
 ?>
